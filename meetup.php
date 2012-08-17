ace7b0f34e12090a852b3437384fdd3d535aced9
Tim Lytle <tim@timlytle.net>
2012-08-17 12:18:51 -0400

    Converted site from HTML with PHP, to a PHP script that renders a template.

---
+++ meetup.php
@@ -0,0 +1,52 @@
+<?php
+class Meetup
+{
+    const API_ENDPOINT = 'https://api.meetup.com/2/';
+    const API_GROUPS = 'groups';
+    
+    protected $key;
+    protected $defaults = array();
+    
+    public function __construct($key)
+    {
+        $this->key = $key;
+        $this->defaults['key'] = $this->key;
+    }
+    
+    /**
+     * Get an array of group data from Meetup.
+     * @param string $group
+     * @return array
+     */
+    public function getGroup($group)
+    {
+        //quey the api
+        $result = $this->makeRequest(self::API_ENDPOINT . self::API_GROUPS, array('group_id' => $group));
+        //check for the expected result
+        if(!isset($result['results'][0])){
+            throw new Exception('bad response from API');
+        }
+        
+        //return the date
+        return $result['results'][0];
+    }
+    
+    /**
+     * Simple HTTP Request that parses a JSON response.
+     * 
+     * @param string $url
+     * @param array $params
+     */
+    protected function makeRequest($url, $params = array())
+    {
+        //set defaults
+        $params = array_merge($this->defaults, $params);
+        //build the query string
+        $request = $url . '?' . http_build_query($params);
+        //request the data
+        $response = file_get_contents($request);
+        //parse and return an array
+        return json_decode($response, true);
+    }
+    
+}
